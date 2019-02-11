6a0f633c9bed1e9bc104e7b1af7ecb7483e2c77e
Henry Neal <henry.neal0@gmail.com>
2019-02-11 15:54:23 -0500

    new branch commit update operation not working

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const db = require('./data/db')
+
+
+server.get
+
+// U
+server.post('/users', (req, res) => {
+    const {user} = req.body;
+    
+    db.users
+    .add(user)
+    .then(user => {
+        res.status(201).json({success:true, user});
+    })
+    .catch(({code, message}) => {
+        res.status(code).json({success: false, message});
+    }); 
+});
+
+server.listen(4000, () => {
+    console.log('\n*** running on port 4000 ***\n');
+});
